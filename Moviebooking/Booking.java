d15c29a5bd6fef1128de2cf61c5296df9f6c9383
dev-saravana <83128745+dev-saravana@users.noreply.github.com>
2022-06-29 12:31:10 +0530

    read Readme file for instruction

---
+++ Moviebooking/Booking.java
@@ -0,0 +1,34 @@
+package Moviebooking;
+//import java.time.*;
+import java.sql.*;
+
+
+public class Booking {
+	String movie;
+	String name;
+	int age;
+	int ticket;
+	String date;
+	public Booking(String movie,String name,int age,int tickets) {
+		this.movie=movie;
+		this.name=name;
+		this.age=age;
+		this.ticket=tickets;
+	}
+	public void setdata() throws SQLException {
+		String query="insert into Booking values(?,?,?,?,?)";
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Movies ","root","root@1234");
+		PreparedStatement st=con.prepareStatement(query);
+		st.setString(1,movie);
+		st.setString(2,name);
+		st.setInt(3, age);
+		java.util.Date d=new java.util.Date();
+		String s=d.toString();
+		//long d=System.currentTimeMillis();
+		//java.sql.Date dt=new java.sql.Date(d);
+		st.setString(4,s);
+		st.setInt(5, ticket);
+	    st.executeUpdate();
+	}
+
+}
